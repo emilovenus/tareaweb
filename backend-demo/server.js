2720a62fb59d6530e0a9186376d4b193943f7ad3
emilovenus <loreemily8@icloud.com>
2025-11-18 10:29:00 -0700

    mensaje

---
+++ backend-demo/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+const cors = require("cors");
+
+app.use(cors());
+
+app.get("/api/mensaje", (req, res) => {
+  res.json({ mensaje: "Hola desde el backend!" });
+});
+
+app.listen(port, () => console.log("Servidor funcionando en puerto " + port));
